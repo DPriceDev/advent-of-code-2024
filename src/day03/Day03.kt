00988ccf93a92ee27adc9e5cc39676a6a2fc6be6
David Price <DPriceDev@gmail.com>
2024-12-03 10:17:34 +0000

    Fay03: swap to array get

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -6,7 +6,7 @@ fun part2(input: String) = DO_DON_T_MULTIPLY_REGEX.findAll(input).fold(initial =
 
 private fun parseCommand(previous: Pair<Long, Boolean>, match: MatchResult): Pair<Long, Boolean> {
     val (sum, isEnabled) = previous
-    val (command) = match.destructured
+    val command = match.groupValues[1]
     return when {
         command == "don't" -> sum to false
         command == "mul" && isEnabled -> sum + match.groupValues.drop(2).let(::multiply) to true
