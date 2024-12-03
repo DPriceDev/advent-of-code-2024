67a708f975ea78eccd6632a2e8a3522cc703266d
David Price <DPriceDev@gmail.com>
2024-12-03 10:16:01 +0000

    added clarifying label

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -2,7 +2,7 @@ package day03
 
 fun part1(input: String) = MULTIPLY_REGEX.findAll(input).map { it.groupValues.drop(1) }.sumOf(::multiply)
 
-fun part2(input: String) = DO_DON_T_MULTIPLY_REGEX.findAll(input).fold(0L to true, ::parseCommand).first
+fun part2(input: String) = DO_DON_T_MULTIPLY_REGEX.findAll(input).fold(initial = 0L to true, ::parseCommand).first
 
 private fun parseCommand(previous: Pair<Long, Boolean>, match: MatchResult): Pair<Long, Boolean> {
     val (sum, isEnabled) = previous
